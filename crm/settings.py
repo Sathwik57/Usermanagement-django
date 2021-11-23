229d0f066326335549d31bacd59c4db8548a5000
Sathwik57 <86978530+Sathwik57@users.noreply.github.com>
2021-11-23 17:37:26 +0530

    Update settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -144,6 +144,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tempdjango1@gmail.com'
-# print(os.environ.get('EMAIL_ID'))
-EMAIL_HOST_PASSWORD = 'rqagyfybqmttywkp'
+EMAIL_HOST_USER = os.environ.get('EMAIL_ID')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PSWD')
